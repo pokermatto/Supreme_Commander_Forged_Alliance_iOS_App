27ea4be3672468295a488cf89297b0551ddce80f
Mitch Hickox <mitchhickox@gmail.com>
2017-05-16 15:30:45 -0400

    Unit Info Controller Setup

--- Supreme_Commander_Forged_Alliance_iOS_App/DetailViewController.swift
+++ Supreme_Commander_Forged_Alliance_iOS_App/DetailViewController.swift
@@ -43,15 +43,15 @@ class DetailViewController: UIViewController , UITableViewDataSource, UITableVie
     //Transitions to Detail View on UITableViewCell Click
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail2" {
+           
             if detailTableView.indexPathForSelectedRow != nil
             {
                 
-                //let currentCell = detailTableView.cellForRow(at: detailTableView.indexPathForSelectedRow!)!
-                //selectedLabel = currentCell.textLabel!.text
-                //let object = objects[indexPath.row] as! NSDate
+                let currentCell = detailTableView.cellForRow(at: detailTableView.indexPathForSelectedRow!)!
+                let selectedLabel = currentCell.textLabel!.text
                 let controller = (segue.destination as! UINavigationController).topViewController as! InfoViewController
-                //controller.detailItem
-                //controller.dataPassed = selectedLabel
+                
+                controller.unitName = selectedLabel
                 controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
                 controller.navigationItem.leftItemsSupplementBackButton = true
             }
--- Supreme_Commander_Forged_Alliance_iOS_App/InfoViewController.swift
+++ Supreme_Commander_Forged_Alliance_iOS_App/InfoViewController.swift
@@ -8,33 +8,89 @@
 
 import UIKit
 
-class InfoViewController: UIViewController {
-
+class InfoViewController: UIViewController , UITableViewDataSource, UITableViewDelegate {
+    //, UITableViewDataSource, UITableViewDelegate
+    var unitName: String!
     
+    
+    var detailItem: NSDate?
     @IBOutlet weak var topLabel: UINavigationItem!
     
     @IBOutlet weak var detailTableView: UITableView!
-
+    
+    
+    
+    var engineers: [String] = ["T1 Engineer","T2 Engineering Station: The Kennel", "T2 Engineer","T2 Field Engineer: Sparky","T3 Engineering Station: The Kennel", "T3 Engineer"]
+    
+    func configureView() {
+        // Update the user interface for the detail item.
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
+        detailTableView.delegate = self
+        detailTableView.dataSource = self
+        
+        configureView()
+        print(unitName)
+        topLabel.title = "UNITNAME"
+        
+        
     }
-
+    
+  
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    
+    //Returns the height of the section
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 0
     }
-    */
-
+    
+    
+    //Returns the number of sections
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        //DF return 1
+        return 1
+    }
+    
+    
+    //Returns number of rows in a given section
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        //DF return objects.count
+        /*CURRENTLY NOT PASSED
+         
+         if dataPassed == "Engineer"
+        {
+            return engineers.count
+        }*/
+        return 0
+    }
+    
+    //Creates table cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        //DF let object = objects[indexPath.row] as! NSDate
+        //DF cell.textLabel!.text = object.description
+        cell.textLabel?.text = engineers[indexPath.row]
+        return cell
+    }
+    
+    
+    
+    
+    
+    
+    
 }
+
