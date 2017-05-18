cd4e71f6952f77237cfb5ad238543e02df9131e6
Mitch Hickox <mitchhickox@gmail.com>
2017-05-17 20:24:42 -0400

    Added image view and Auto Layout constraints to info view

--- Supreme_Commander_Forged_Alliance_iOS_App/InfoViewController.swift
+++ Supreme_Commander_Forged_Alliance_iOS_App/InfoViewController.swift
@@ -10,12 +10,7 @@ import UIKit
 
 class InfoViewController: UIViewController , UITableViewDataSource, UITableViewDelegate {
  
-    //Title of cell selected on DetailViewController
-    var unitName: String!
-    
-    //TODO delete this var, unused, ??problem??: still required to be kept here by other xcode file
-    var detailItem: NSDate?
-    
+    // MARK: - IBOutlets
     //Outlet to label at top of InfoViewController in storyboard
     @IBOutlet weak var topLabel: UINavigationItem!
     
@@ -24,7 +19,56 @@ class InfoViewController: UIViewController , UITableViewDataSource, UITableViewD
     
     
     
-    var engineers: [String] = ["T1 Engineer","T2 Engineering Station: The Kennel", "T2 Engineer","T2 Field Engineer: Sparky","T3 Engineering Station: The Kennel", "T3 Engineer"]
+    // MARK: - Variables
+    //Title of cell selected on DetailViewController
+    var unitName: String!
+    
+    //Category Data filled in by setTableDataForSelectedUnit(_ nameOfUnit: String!) method
+    var unitSectionNames: [String] = []
+    var unitSectionCells: [[String]] = []
+    
+    
+    
+    //var engineers: [String] = ["T1 Engineer","T2 Engineering Station: The Kennel", "T2 Engineer","T2 Field Engineer: Sparky","T3 Engineering Station: The Kennel", "T3 Engineer"]
+    
+    
+    
+    
+    //Set variables unitSectionNames and unitSectionCells according to the passed unit name
+    func setTableDataForSelectedUnit(_ nameOfUnit: String!)
+    {
+        if unitName != nil
+        {
+            switch nameOfUnit
+            {
+                
+            case "T1 Engineer":
+                unitSectionNames = ["Blueprint ID","Health", "Abilities", "Economy", "Intel" , "Physics", "Wreckage"]
+                let bluePrintID = ["UE10105"]
+                let health: [String] =  ["HP: 150", "Regen Rate: 0 HP/s", "Armor Type: Normal"]
+                let abilities: [String] =  ["Amphibious", "Engineering Suite"]
+                let economy: [String] =  ["Mass: 53", "Mass Storage: 10", "Energy: 260", "Build Time: 260", "Build Rate: 5", "Build Radius: 5"]
+                let intel: [String] =  ["Vision Radius: 18"]
+                let physics: [String] =  ["Max Speed: 1.9", "Turn Rate: 120"]
+                let wreckage: [String] =  ["Health: 135 HP", "Mass: 46"]
+                unitSectionCells = [bluePrintID, health, abilities, economy, intel, physics, wreckage]
+            default:
+                print("Warning: Name of unit Category Not Found in setTableDataForSelectedUnit(_ nameOfUnit: String!)")
+                
+            }
+            
+        }
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     //TODO possibly delete
     func configureView() {
@@ -40,6 +84,9 @@ class InfoViewController: UIViewController , UITableViewDataSource, UITableViewD
         //Set up TableView delegate and datasource as self
         detailTableView.delegate = self
         detailTableView.dataSource = self
+        
+        //Set TableView data based on unit name
+        setTableDataForSelectedUnit(unitName)
         
         //TODO possibly delete
         configureView()
@@ -70,8 +117,28 @@ class InfoViewController: UIViewController , UITableViewDataSource, UITableViewD
     //Returns the number of sections
     func numberOfSections(in tableView: UITableView) -> Int {
         
-        //DF return 1
+        //Gets number of sections if it is greater than 0
+        if(unitSectionNames.count > 0)
+        {
+            return unitSectionNames.count
+        }
+        
+        //return 1 in case no data is passed (should not happen once all data has been filled in)
         return 1
+    }
+    
+    //Returns name of the section
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        //Gets name of section (if none is present, this method uses default value at end of this method)
+        if(unitSectionNames.count > 0)
+        {
+            return unitSectionNames[section]
+        }
+        
+        
+        //Default value if no unit category matches
+        return "ERROR: NO SECTION NAME FOUND"
     }
     
     
@@ -79,20 +146,29 @@ class InfoViewController: UIViewController , UITableViewDataSource, UITableViewD
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
 
-        /*CURRENTLY NOT PASSED
-         
-         if dataPassed == "Engineer"
+        // Return number of rows in the section based on the section number and count of the array for the unit category currently shown
+        if !unitSectionCells.isEmpty
         {
-            return engineers.count
-        }*/
-        return 0
+            return unitSectionCells[section].count
+        }
+        else
+        {
+            return 0
+        }
     }
     
     //Creates table cell
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //Create cell based on prototype
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
-        cell.textLabel?.text = engineers[indexPath.row]
+        
+        //Set new cell's label based on row/section location (i.e. indexPath)
+        cell.textLabel?.text = unitSectionCells[indexPath.section][indexPath.row]
+        
+        
+        //Return the newly-created cell
         return cell
     }
     
